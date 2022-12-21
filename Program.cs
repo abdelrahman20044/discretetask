8c94083836fafe050dd2b838e22b1fdc4ab676aa
abdelrahman20044 <121033553+abdelrahman20044@users.noreply.github.com>
2022-12-21 15:12:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("please enter the first num");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("please enter the second num");
+int num2 = int.Parse(Console.ReadLine());
+for (int i = num1; i <= num2; i++)
+{
+    int j;
+    for (j = 2; j <= i; j++)
+    {
+        if (i % j == 0)
+        {
+         break;
+        }
+    }
+        if (j == i)
+        {
+            Console.WriteLine(i);
+        }
+    
+
+}
+
