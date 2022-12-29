a4334caed1fa32b27ebdfd6cc77d8b6c142eef2f
abdelrahman20044 <121033553+abdelrahman20044@users.noreply.github.com>
2022-12-29 15:39:02 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,20 @@
-﻿Console.WriteLine("please enter the first num");
-int num1 = int.Parse(Console.ReadLine());
-Console.WriteLine("please enter the second num");
-int num2 = int.Parse(Console.ReadLine());
-for (int i = num1; i <= num2; i++)
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("please enter the first number");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("please enter the second number");
+int second = int.Parse(Console.ReadLine());
+for (int i = first; i <= second; i++)
 {
-    int j;
-    for (j = 2; j <= i; j++)
+    int sum = 0;
+    for (int j = 1; j < i; j++)
     {
         if (i % j == 0)
         {
-         break;
+            sum += j;
         }
     }
-        if (j == i)
-        {
-            Console.WriteLine(i);
-        }
-    
-
+    if (sum == i)
+    {
+        Console.WriteLine(i );
+    }
 }
-
